4f51895cd03ce878230f9db83c5d923af996df30
zajacmp3 <deejay.zajac@gmail.com>
2014-04-01 03:58:15 +0200

    Poprawki konfiguracyjne JS. Czyżby w wersji development rails załączał zawsze cały katalog js?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+require jquery-1.7.1.min
+require jquery.infinitescroll.min
+require jquery.isotope.min
 //= require_tree .
