71258b8be8575997105ed1f862ac03b506c24098
zajacmp3 <deejay.zajac@gmail.com>
2014-04-03 11:13:16 +0200

    Dodanie seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+todos = Todo.create([{id: '1', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '1', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '2', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '3', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '4', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '5', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '6', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '7', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '8', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '9', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '10', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '11', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '12', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '13', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '14', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '15', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '16', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'},
+	{id: '17', name: 'Przykładowy wpis', description: 'Przykładowy opis', image: '', remote_image: '', created_at: '2014-03-27 19:46:27.176948', updated_at: '2014-03-27 19:46:27.176948'}
+
+	])
